99246cf71ace99181f06d2930893f7f560b8d7c4
Jekman87 <jeka_zzz@mail.ru>
2024-03-18 06:15:03 +0300

    feat: implement FavoritesModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { DatabaseModule } from './database/database.module';
 import { TrackModule } from './track/track.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
+import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { AlbumModule } from './album/album.module';
     ArtistModule,
     AlbumModule,
     DatabaseModule,
+    FavoritesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
